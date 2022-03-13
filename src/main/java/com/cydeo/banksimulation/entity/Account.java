98c4af87c231243833fafc57ea204fbe7e7a061d
UmitUmit <umit.green@gmail.com>
2022-03-12 23:32:37 -0500

    Bank Simulation - 01 SpringBoot Steps

---
+++ src/main/java/com/cydeo/banksimulation/entity/Account.java
@@ -0,0 +1,21 @@
+package com.cydeo.banksimulation.entity;
+
+import com.cydeo.banksimulation.enums.AccountType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Builder
+public class Account {
+    private UUID id;
+    private BigDecimal balance;
+    private AccountType accountType;
+    private Date creationDate;
+    private long userId;
+
+}
